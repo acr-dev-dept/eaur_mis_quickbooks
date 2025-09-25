3be88dc76b93c1285d9981bfb8b449ad75f1bf9f
rugem3 <rugema61@gmail.com>
2025-09-25 07:25:22 +0200

    created a celery utility module seperately

---
+++ application/utils/celery_utils.py
@@ -0,0 +1,20 @@
+from celery import Celery
+
+def make_celery(app=None):
+    celery = Celery(
+        __name__,
+        broker=app.config.get("CELERY_BROKER_URL", "redis://localhost:6379/0"),
+        backend=app.config.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+    )
+    if app:
+        # Copy config from Flask app to Celery
+        celery.conf.update(app.config)
+
+        # Bind Flask context to Celery tasks
+        class ContextTask(celery.Task):
+            def __call__(self, *args, **kwargs):
+                with app.app_context():
+                    return self.run(*args, **kwargs)
+        celery.Task = ContextTask
+
+    return celery
