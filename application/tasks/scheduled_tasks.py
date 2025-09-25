0d5064e079cb6698fbaaecb925135c4411633b42
rugem3 <rugema61@gmail.com>
2025-09-25 09:01:13 +0200

    added the scheduled tasks

---
+++ application/tasks/scheduled_tasks.py
@@ -0,0 +1,17 @@
+from celery import Celery
+from application.tasks.quickbooks_sync import celery as payment_celery
+from application.tasks.customer_sync_tasks import celery as customer_celery
+from celery.schedules import crontab
+
+# Pick one Celery instance to configure beat (usually the main one)
+celery = payment_celery  # you can also make a dedicated celery instance if you prefer
+
+celery.conf.beat_schedule = {
+    "sync-payments-every-5-min": {
+        "task": "application.tasks.quickbooks_sync.sync_payments",
+        "schedule": 300.0,  # every 5 minutes
+        "args": (),
+    }
+}
+
+celery.conf.timezone = "Africa/Kigali"
