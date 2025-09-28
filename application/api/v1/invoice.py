6a40d9129951fdee73068f557a80c4fa4726bc35
kabandapatrick2580 <sendsome2019@gmail.com>
2025-09-29 01:34:25 +0200

    html invoice skeleton

--- application/api/v1/invoice.py
+++ application/api/v1/invoice.py
@@ -650,3 +650,9 @@ def get_mis_invoices():
             "recordsFiltered": 0,
             "data": []
         })
+
+# HTML page route
+@invoices_bp.route('/invoices', methods=['GET'])
+def invoices_page():
+    """Render the invoices page with empty table skeleton"""
+    return render_template("dashboard/invoices.html")
