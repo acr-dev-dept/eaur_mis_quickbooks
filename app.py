85a856c9bc3827ab26bfdf5c21c92db6172c6d29
kabandapatrick2580 <sendsome2019@gmail.com>
2025-10-28 09:51:22 +0200

    updated the port to operate simultaneously

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_ENV') == 'development'
     app.run(
         host=os.getenv('FLASK_HOST', '0.0.0.0'),
-        port=int(os.getenv('FLASK_PORT', 9000)),
+        port=8000,
         debug=debug_mode
     )
