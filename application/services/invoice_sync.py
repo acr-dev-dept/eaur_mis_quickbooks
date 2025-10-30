07f34a72895ad1d6209bccf99072643b684d3b99
kabandapatrick2580 <sendsome2019@gmail.com>
2025-10-30 09:46:46 +0200

    updated from LocationRef to Departmntref

--- application/services/invoice_sync.py
+++ application/services/invoice_sync.py
@@ -320,7 +320,7 @@ class InvoiceSyncService:
                             "ClassRef": {
                                 "value": int(class_ref_id) if class_ref_id else ''  # must exist in QB
                             },
-                            "LocationRef": {
+                            "DepartmentRef": {
                                 "value": int(location_id) if location_id else ''  # must exist in QB
                             },
                             "Qty": 1,
