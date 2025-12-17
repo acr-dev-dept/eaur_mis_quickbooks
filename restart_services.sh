1d986a270a25717ddd6fdfdd25c83df84a2b0903
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-17 12:49:37 +0200

    removed space in Q

--- restart_services.sh
+++ restart_services.sh
@@ -48,7 +48,7 @@ fi
 
 echo "Starting Celery worker..."
 nohup celery -A application.config_files.celery worker \
-    -Q celery, payment_sync_queue \
+    -Q celery,payment_sync_queue \
     --loglevel=info > "$LOG_DIR/celery_worker.log" 2>&1 &
 sleep 2
 if pgrep -f "celery worker" > /dev/null; then
