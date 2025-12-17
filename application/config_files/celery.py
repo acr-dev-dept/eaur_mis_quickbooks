6e4c9563c477010d0bf5cdb8e7f2665c0d33d73a
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-17 11:17:12 +0200

    included the context again

--- application/config_files/celery.py
+++ application/config_files/celery.py
@@ -8,7 +8,7 @@ load_dotenv()
 celery = Celery(
     'my_app',
     broker=os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0'),
-    backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
+    backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/1'),
     include=['application.config_files.tasks']
 )
 
@@ -28,7 +28,13 @@ celery.conf.beat_schedule = {
 celery.conf.task_routes = {
     'application.config_files.payment_sync.sync_payment_to_quickbooks_task': {'queue': 'payment_sync_queue'},
 }
-
 def make_celery(app):
     celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
     return celery
