db8946b05476842de42253f48c564f3a1b356934
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-18 16:22:41 +0200

    send task by its full python task

--- application/api/v1/urubuto.py
+++ application/api/v1/urubuto.py
@@ -400,11 +400,8 @@ def payment_callback():
                         # Use the endpoint to sync single payment
                         # url = f"https://api.eaur.ac.rw/api/v1/sync/payments/sync_payment/{payment_id}"
                         
-                        response = current_app.celery.send_task("application.config_files.payment_sync.sync_payment_to_quickbooks_task", args=[payment_id])
-                        if response:
-                            current_app.logger.info(f"Payment {response} sync to QuickBooks task queued successfully: {response.id}")
-                        else:
-                            current_app.logger.error(f"Payment {payment} sync to QuickBooks task failed to queue:")
+                        current_app.celery.send_task("application.config_files.payment_sync.sync_payment_to_quickbooks_task", args=[payment_id])
+                        
                     except Exception as e:
                         current_app.logger.error(f"Error syncing payment {payment} to QuickBooks: {str(e)}")
                         current_app.logger.error(traceback.format_exc())
