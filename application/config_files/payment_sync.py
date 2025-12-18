ad89072ef02150b5f84b4d74f60681887aa1a101
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-18 12:53:24 +0200

    reconfig

--- application/config_files/payment_sync.py
+++ application/config_files/payment_sync.py
@@ -1,18 +1,15 @@
-# from shared_task
-# from celery import shared_task
 from application.config_files.celery import celery
 from application.services.payment_sync import PaymentSyncService
-from application import create_app as app
+
 
 @celery.task(
     bind=True,
     autoretry_for=(Exception,),
-    retry_backoff=60,  # retry after 1 minute
+    retry_backoff=60,
     retry_kwargs={"max_retries": 5},
     retry_jitter=True,
-    name="application.config_files.payment_sync.sync_payment_to_quickbooks_task"  # explicit name
 )
 def sync_payment_to_quickbooks_task(self, payment_id: int):
-    with app.app_context():
-        service = PaymentSyncService()
-        return service.sync_single_payment_async(payment_id)
+    # No need to create app or push context — it's automatic!
+    service = PaymentSyncService()
+    return service.sync_single_payment_async(payment_id)
