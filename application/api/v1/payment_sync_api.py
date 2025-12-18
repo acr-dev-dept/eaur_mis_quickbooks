277976793dfbe645bde23aa00c49920128b9de83
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-18 13:08:38 +0200

    datetime str issues

--- application/api/v1/payment_sync_api.py
+++ application/api/v1/payment_sync_api.py
@@ -34,8 +34,9 @@ def sync_payment(payment_id):
         current_app.logger.info(f"Starting sync for payment ID: {payment_id}")
         # get the payment object given the payment_id
         payment = Payment.get_payment_by_id(payment_id)
+        payment_date = datetime.strptime(payment.date.strip(), '%d/%m/%Y').date()
 
-        if payment.date < datetime(2025, 1, 1):
+        if payment_date < datetime(2025, 1, 1):
             return jsonify({'error': 'Payment date is before 2025-01-01'}), 400
 
         if not payment:
