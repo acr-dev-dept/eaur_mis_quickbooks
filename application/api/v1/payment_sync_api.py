92f185673a09f31c65bf22d80d2a6daf15e482a0
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-18 13:14:33 +0200

    date objects fixed

--- application/api/v1/payment_sync_api.py
+++ application/api/v1/payment_sync_api.py
@@ -34,9 +34,9 @@ def sync_payment(payment_id):
         current_app.logger.info(f"Starting sync for payment ID: {payment_id}")
         # get the payment object given the payment_id
         payment = Payment.get_payment_by_id(payment_id)
-        payment_date = datetime.strptime(payment.date.strip(), '%Y/%m/%d').date()
+        payment_date = datetime.strptime(payment.date.strip(), '%Y-%m-%d').date()
 
-        if payment_date < datetime(2025, 1, 1):
+        if payment_date < datetime(2025, 1, 1).date():
             return jsonify({'error': 'Payment date is before 2025-01-01'}), 400
 
         if not payment:
