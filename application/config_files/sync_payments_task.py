6c441cba90e7c325e598e91102902d2db8d7a4c8
kabandapatrick2580 <sendsome2019@gmail.com>
2025-12-31 12:59:49 +0200

    sync payments

--- application/config_files/sync_payments_task.py
+++ application/config_files/sync_payments_task.py
@@ -51,7 +51,7 @@ def bulk_sync_payments_task(payment_ids=None, batch_size=50, filter_unsynced=Tru
                 else:
                     raise ValueError("No payments left to process")
 
-                payment_ids = [p.payment_id for p in payments if p.payment_id is not None]
+                payment_ids = [p.id for p in payments if p.id is not None]
             else:
                 current_offset = None
 
