c99a2d4f103990fa0b50b59de1804546194b6f60
kabandapatrick2580 <sendsome2019@gmail.com>
2026-01-01 09:43:05 +0200

    fixed datetime error

--- application/api/v1/urubuto.py
+++ application/api/v1/urubuto.py
@@ -475,19 +475,20 @@ def payment_callback():
                 # Create wallet
                 from application.models.mis_models import TblStudentWallet
                 import random
+                from datetime import date
                 try:
-                    updated = TblStudentWallet.create_wallet_entry(
+                    created = TblStudentWallet.create_wallet_entry(
                         reg_prg_id=random.randint(100000, 999999),
                         reg_no=student.reg_no,
                         reference_number=random.randint(100000, 999999),
                         trans_code=transaction_id,
                         payment_chanel=payment_chanel,
-                        payment_date=payment_date_time,
+                        payment_date=date.today(),
                         is_paid="Yes",
                         dept=amount,
                         bank_id=2
                     )
-                    current_app.logger.info(f"Wallet created: {updated}")
+                    current_app.logger.info(f"Wallet created: {created}")
                 except Exception as e:
                     current_app.logger.error(f"Error creating wallet entry: {str(e)}")
                     current_app.logger.error(traceback.format_exc())
