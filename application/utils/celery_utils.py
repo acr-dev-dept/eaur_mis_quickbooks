5403bcd10d08d2d19137a31d6904671a9aa5f5db
kabandapatrick2580 <sendsome2019@gmail.com>
2026-01-23 20:56:29 +0200

    added weekend schedules

--- application/utils/celery_utils.py
+++ application/utils/celery_utils.py
@@ -82,7 +82,7 @@ def make_celery(app):
         "task": "application.config_files.tasks.bulk_sync_applicants_task",
         "schedule": crontab(
             minute='6,42',
-            hour='0-16',
+            hour='6-16',
             day_of_week='sat,sun'
             ),
         },
@@ -90,7 +90,7 @@ def make_celery(app):
         "task": "application.config_files.sync_students_task.bulk_sync_students_task",
         "schedule": crontab(
             minute='6,42',
-            hour='0-16',
+            hour='6-16',
             day_of_week='sat,sun'
             ),
         },
@@ -98,7 +98,7 @@ def make_celery(app):
         "task": "application.config_files.sync_invoices_task.scheduled_invoice_sync_task",
         "schedule": crontab(
             minute='12,48',
-            hour='0-16',
+            hour='6-16',
             day_of_week='sat,sun'
             ),
         },
@@ -106,7 +106,7 @@ def make_celery(app):
         "task": "application.config_files.sync_sales_receipt_task.scheduled_sales_receipt_sync_task",
         "schedule": crontab(
             minute='18,54',
-            hour='0-16',
+            hour='6-16',
             day_of_week='sat,sun'
             ),
         },
@@ -114,7 +114,7 @@ def make_celery(app):
         "task": "application.config_files.update_invoices_task.scheduled_invoice_update_task",
         "schedule": crontab(
             minute='24,57',
-            hour='0-16',
+            hour='6-16',
             day_of_week='sat,sun'
             ),
         },
@@ -122,7 +122,7 @@ def make_celery(app):
         "task": "application.config_files.sync_payments_task.bulk_sync_payments_task",
         "schedule": crontab(
             minute='30,54',
-            hour='0-16',
+            hour='6-16', 
             day_of_week='sat,sun'
             ),
         },
