db8e878ff345db30d7fab0ff9df7e529e9ee5cd2
kabandapatrick2580 <sendsome2019@gmail.com>
2026-01-25 23:54:58 +0200

    sync update invoices

--- application/utils/celery_utils.py
+++ application/utils/celery_utils.py
@@ -74,18 +74,18 @@ def make_celery(app):
             },
             "sync_invoices_every_midnight": {
             "task": "application.config_files.sync_invoices_task.scheduled_invoice_sync_task",
-            "schedule": crontab(minute='12,24,36,48,54',
+            "schedule": crontab(minute='12,24,36',
                                 hour="0,2,4,6,8,10,12,14,16,18,20,22",
                                 day_of_week='mon,tue,wed,thu,fri')
             },
             "update_invoices": {
             "task": "application.config_files.update_invoices_task.scheduled_invoice_update_task",
             "schedule": crontab(
-                minute='6,30,54,24,57',
+                minute='6,24,30',
                 hour='1,3,5,7,9,11,13,15,17,19,21,23',
                 day_of_week='mon,tue,wed,thu,fri'
             )
-            },
+        },
 
 
 
