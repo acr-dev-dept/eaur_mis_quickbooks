5bdc0ca6636be5a3f0006b5d4c6f97073abf3e19
kabandapatrick2580 <sendsome2019@gmail.com>
2026-02-02 01:41:25 +0200

    task to delete sales receipts

--- application/tasks/delete_sales_receipt_master.py
+++ application/tasks/delete_sales_receipt_master.py
@@ -4,9 +4,7 @@ from celery import shared_task, group
 from application import create_app, db
 from application.models.mis_models import TblStudentWallet
 from application.models.central_models import QuickBooksConfig
-from application.tasks.delete_sales_receipt_single import (
-    delete_single_wallet_sales_receipt,
-)
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +27,9 @@ def delete_all_wallet_sales_receipts_master():
         ]
 
         logger.info("Dispatching %s deletion tasks", len(wallet_ids))
-
+        from application.tasks.delete_sales_receipt_single import (
+            delete_single_wallet_sales_receipt,
+        )
         job = group(
             delete_single_wallet_sales_receipt.s(wallet_id)
             for wallet_id in wallet_ids
